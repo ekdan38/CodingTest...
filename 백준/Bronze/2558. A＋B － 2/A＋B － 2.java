1af97fc5509fafff81e7709b67ba15c8261d17e1
JeongHwa <103169652+ekdan38@users.noreply.github.com>
2024-03-16 23:32:07 +0900

    [Bronze V] Title: A+B - 2, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,20 @@
+
+import java.io.BufferedReader;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		
+		System.out.println(A + B);
+
+	}	
+}
