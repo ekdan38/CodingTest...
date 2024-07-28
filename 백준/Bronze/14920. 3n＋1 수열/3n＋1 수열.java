9b1d9e5c863369fdad7e2cee2c7423dd97a9b213
JeongHwa <103169652+ekdan38@users.noreply.github.com>
2024-07-28 12:54:45 +0900

    [Bronze III] Title: 3n+1 수열, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/14920. 3n＋1 수열/3n＋1 수열.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+
+        long seqNum = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+
+        while (cnt < Integer.MAX_VALUE) {
+            cnt++;
+            if (seqNum == 1) {
+                break;
+            }
+            if (seqNum % 2 == 1) {
+                seqNum = seqNum * 3 + 1;
+            }
+            else {
+                seqNum = seqNum / 2;
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
+
