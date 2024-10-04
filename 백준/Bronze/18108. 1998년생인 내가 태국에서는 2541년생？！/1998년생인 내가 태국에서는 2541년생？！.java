495c2d7be7dcef4465e3f90ab8b3eb5829ec0e87
JeongHwa <103169652+ekdan38@users.noreply.github.com>
2024-10-04 12:16:21 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 11576 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int year = Integer.parseInt(bf.readLine()) - 543;
+        bw.write(String.valueOf(year));
+        bw.flush();
+        bw.close();
+    }
+}
