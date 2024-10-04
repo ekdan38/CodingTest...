f35707f71cac1da099108df79e48313c9b130d43
JeongHwa <103169652+ekdan38@users.noreply.github.com>
2024-10-04 17:26:02 +0900

    [Bronze II] Title: 공 바꾸기, Time: 64 ms, Memory: 11832 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 바구니 1~N
+        // M번 공을 넣는다. 공을 넣을때 버ㅁ위를 지정한다. 지정한 바구니에 모두 같은 번호의 공을 넣ㄴ느다.
+        // 바구니에 공이 있으면 공을 모두 빼고 색공을 ㄴ허는다.
+        // i j k => i~ j까지 k를 넣는다.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] basket = new int[N];
+
+        int ballNum = 1;
+        for(int i = 0; i < basket.length; i++){
+            basket[i] = ballNum++;
+        }
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken()) - 1;
+            int end = Integer.parseInt(st.nextToken()) - 1;
+            int temp = basket[start];
+            basket[start] = basket[end];
+            basket[end] = temp;
+        }
+
+        for(int ball : basket){
+            bw.write(ball + " ");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
