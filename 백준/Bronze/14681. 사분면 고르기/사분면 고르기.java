7c5d2045e4471a2c18327ad476b3f6f6075522c7
JeongHwa <103169652+ekdan38@users.noreply.github.com>
2024-10-04 13:39:43 +0900

    [Bronze V] Title: 사분면 고르기, Time: 64 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        //연도가 주어졌을 때, 윤년이면 1, 아니면 0을 출력하는 프로그램을 작성하시오.
+        //
+        //윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+
+        if(x > 0 && y > 0)bw.write("1");
+        else if(x < 0 && y > 0)bw.write("2");
+        else if(x < 0 && y < 0)bw.write("3");
+        else if(x > 0 && y < 0)bw.write("4");
+
+        bw.flush();
+        bw.close();
+    }
+}
