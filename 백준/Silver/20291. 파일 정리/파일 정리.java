dc4e83fb6fb52a45f293af7330bf83969d94e894
JeongHwa <103169652+ekdan38@users.noreply.github.com>
2024-11-15 09:52:30 +0900

    [Silver III] Title: 파일 정리, Time: 684 ms, Memory: 44144 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    static int[] dp = new int[1001];    // 메모이제이션을 위한 배열
+
+    public static void main(String[] args) throws IOException {
+        /**
+         * 20291 파일 정리
+         * 파일의 확장자를 찾고, 확장자 명과 개수를 출력해야한다.
+         * 확장자 명은 사전순으로 출력해야한다.
+         * Map중 TreeMap은 오름차순을 보장해준다. HashMap을 사용하고 stream을 돌려 사전순으로 정렬해도 되지만
+         * TreeMap을 사용하여 풀어보겠다.
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        // 파일 이름, 개수 저장
+        Map<String, Integer> extentionMap = new TreeMap<>();
+        for(int i = 0; i < N; i++){
+            String fileName = br.readLine();
+            String extention = fileName.substring(fileName.lastIndexOf('.') + 1);
+            extentionMap.put(extention, extentionMap.getOrDefault(extention, 0) + 1);
+        }
+
+        // 정렬된 값 출력
+        for(Map.Entry<String, Integer> entry : extentionMap.entrySet()){
+            bw.write(entry.getKey() + " " + entry.getValue() + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
+
