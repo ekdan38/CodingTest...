1bd7ab16e87230b13b6ace416eb0e59f4443cb3f
JeongHwa <103169652+ekdan38@users.noreply.github.com>
2024-12-04 13:23:24 +0900

    [Silver IV] Title: 30, Time: 160 ms, Memory: 16608 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -12,7 +12,7 @@ import java.util.*;
  *      2. 무작정 반복문 돌려서 하기에는 시간 제한이 있다. N 범위 생각하면 무조건 시간 제한 초과
  *      3. 30의 배수 규칙... => 각 자리 값 더한 값이 3의 배수, 마지막 자리가 0 이다.
  * 출력:
- *      1.
+ *      1. 값 존재하면 출력, 아니면 - 1
  */
 
 public class Main {
@@ -24,13 +24,16 @@ public class Main {
         String str = br.readLine();
         char[] arr = str.toCharArray();
         
-        // 각 자리 더한게 3의 배수인지 확인
+        // 각 자리 더한게 3의 배수인지 확인 해야됨. 합 구해주자
         int sum = 0;
         for(int i = 0; i < str.length(); i++){
             sum += str.charAt(i) - '0';
         }
+        // 오름차순 정렬
         Arrays.sort(arr);
 
+        // 3의 배수인지 확인
+        // 오른차순이기에 arr[0]이 0인지 확인
         if(sum % 3 == 0 && arr[0] == '0'){
             for(int i = arr.length - 1; i >= 0; i--){
                 sb.append(arr[i]);
