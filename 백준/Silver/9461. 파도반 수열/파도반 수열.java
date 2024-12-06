0b20ef22640439bd886a1f86a2a997ad003a0505
JeongHwa <103169652+ekdan38@users.noreply.github.com>
2024-12-06 19:09:50 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -5,17 +5,10 @@ import java.util.*;
  * 9461 파도반 수열
  *
  * 입력:
- *      1. 첫째 줄 테스트 케이스 수 T (1 ~ 1,000)
- *      2. T 만큼 N (1 ~ 100)
  *
  *문제 분석:
- *      1. P(N)을 구해야 한다. 점화식 찾아보자.
- *      1 1 1 2 2 3 4 5 7 9 12 16~
- *      P(N) = P(N - 2) + P(N - 3)
- *      ex) 7 + 9 = 16
  *
  * 출력:
- *      1. 각 테스트 케이스의 P(N) 출력
  */
 
 public class Main {
@@ -24,24 +17,32 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // index 1번 부터 사용, 100까지 만들어야 하기 때문에 long
-        // => 출력 해보니 int 범위 벗어남
-        long arr[] = new long[101];
-        arr[1] = 1;
-        arr[2] = 1;
-        arr[3] = 1;
-
-        // 100 까지 만들어야 한다.
-        for(int i = 4; i < 101; i++){
-            arr[i] = arr[i -2] + arr[i - 3];
-        }
-        
+        // 정삼각형이 나선 모양, 변의 길이는 1로 시작
+        // P(N) 출력
+        // 1 1 1 2 2 3 4 5 7 9
+        // 점화식 : DP(N) = DP(N - 5) + DP(N - 1)
+        // N은 1 ~ 100이다 => 배열 미리 만들어두자.
         int T = Integer.parseInt(br.readLine());
-        // P(N) 받으면서 바로 출력 
-        for(int i = 0; i < T; i++){
-            bw.write(arr[Integer.parseInt(br.readLine())] + "\n");
+        // index 1부터 사용
+        long [] dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+
+        for(int i = 6; i <= 100; i++){
+            dp[i] = dp[i - 5] + dp[i - 1];
         }
 
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 0; i < T; i++){
+            int N = Integer.parseInt(br.readLine());
+            result.append(dp[N]).append("\n");
+        }
+
+        bw.write(result.toString());
         bw.flush();
         br.close();
         bw.close();
