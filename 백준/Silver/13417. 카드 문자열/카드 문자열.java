131e4489c4a7dd42eabbe5d6e162a87f1495a074
JeongHwa <103169652+ekdan38@users.noreply.github.com>
2024-12-07 11:23:51 +0900

    [Silver III] Title: 카드 문자열, Time: 320 ms, Memory: 39576 KB -BaekjoonHub

--- 백준/Silver/13417. 카드 문자열/카드 문자열.java
+++ 백준/Silver/13417. 카드 문자열/카드 문자열.java
@@ -14,8 +14,9 @@ import java.util.*;
  *      그 다음 부터는 가져와서 둔 카드의 왼쪽 or 오른쪽에 둔다.
  *      이 카드들을 이어서 문자열 만든다.
  *      이 문자열 중에 사전순으로 가장 빠른 녀석 출력
+ *      deque 사용!
  * 출력:
- *      1. 거스름돈 동전의 최소 개수, 거슬러 줄 수 없으면 -1
+ *      1. 테스트 케이스마다 사전 순으로 정렬된 값 출력
  */
 
 public class Main {
@@ -42,15 +43,18 @@ public class Main {
             for(int j = 1; j < N; j++){
                 // deque에 1개만 있을때 => 최초 실행
                 if(deque.size() == 1){
+                    // 1개 있는 값이랑 비교해서 크면 맨 뒤로, 아니면 앞으로(사전 순)
                     if(deque.peek() < charArray[j]) deque.offerLast(charArray[j]);
                     else deque.offerFirst(charArray[j]);
                 }
-
+                // deque에 1개 이상 있을때
                 else{
+                    // deque의 맨 앞이랑 비교해서 같거나 작으면 맨뒤로, 아니면 맨 앞으로(사전 순)
                     if(deque.peekFirst() >= charArray[j]) deque.offerFirst(charArray[j]);
                     else deque.offerLast(charArray[j]);
                 }
             }
+            // deque에서 값 꺼냄
             while(!deque.isEmpty()){
                 result.append(deque.poll());
             }
