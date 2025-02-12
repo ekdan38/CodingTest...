37108192184b1f1bffdc24bf4864451f9dee6d89
홍정화 <103169652+ekdan38@users.noreply.github.com>
2025-02-13 00:41:29 +0900

    [level 2] Title: 비밀 코드 해독, Time: 8.92 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -15,52 +15,18 @@
 import java.util.*;
 
 class Solution {
-    static int [] visited;
-    static int result = 0;
-    static int [][] questions;
-    static int [] answers;
-    
     public int solution(int n, int[][] q, int[] ans) {
-        visited = new int[n + 1];
-        questions = q;
-        answers = ans;
-        
         // 길이가 비밀 코드 저장
-        dfs(n, 0, 0);  // DFS 탐색 시작
+        List<int[]> comb = new ArrayList<>();
         
         
         
         
-        return result;
+        return 0;
     }
     // 길이 5인 비밀 코드 생성
-    static void dfs(int n, int depth, int last) {
-        if (depth == 5) { 
-            validate();
-            return;
-        }
-
-        for (int i = last + 1; i <= n; i++) {  // 중복 없이 선택
-            if (visited[i] == 0) {  
-                visited[i] = 1;  // 선택
-                dfs(n, depth + 1, i);  // 다음 숫자 선택 (재귀 호출)
-                visited[i] = 0;  // 선택 해제 (백트래킹)
-            }
-        }
-    }
-
-    static void validate() {
-        for (int i = 0; i < questions.length; i++) {  // 모든 질문을 검사
-            int cnt = 0;
-            for (int j = 0; j < 5; j++) {  // 현재 선택된 숫자 5개 확인
-                if (visited[questions[i][j]] == 1) {  // 선택된 숫자가 있다면 증가
-                    cnt++;
-                }
-            }
-            if (cnt != answers[i]) {  // 기대값과 다르면 실패
-                return;
-            }
-        }
-        result++;  // 모든 질문을 만족하면 정답 개수 증가
+    static void generateSecretCode(){
+        
+        
     }
 }
