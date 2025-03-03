d859a435ab9ea296cb179f984502f5d168716a3a
홍정화 <103169652+ekdan38@users.noreply.github.com>
2025-03-03 19:21:14 +0900

    [Silver III] Title: 블로그, Time: 316 ms, Memory: 35080 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -1,54 +1,61 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
+/**
+ * 21921 블로그
+ *
+ * 입력:
+ *      1. 첫째 줄 블로그를 시작하고 지난 일수 N X 가 공백
+ *      => N : 블로그 시작 한 지난 일수, X : X일 동안 들어온 방문자 수, 기간
+ *      2. 둘째 줄 블로그 시작 1일 차부터 N일차 까지 하루 방문자 수
+ *
+ * 문제 분석:
+ *      1. 블로그를 만든지 N일이 지남, X 일 동안 가장 많이 들어온 방문자 수, 그 기간 구하자
+ *      => 슬라이딩 윈도우 사용
+ * 출력:
+ *      1. 첫째 줄 X 일 동안 가장 많이 들어온 방문자 수 출력 0명이면 "SAD" 출력
+ *      2. 최대 방문자 수가 0명이 아니면 둘째 줄에 기간 출력
+ *
+ * */
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 예제 입력(직접 입력해서 테스트 가능)
-        // 7 3
-        // 1 2 3 2 4 2 1
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        // 첫 줄: N, X
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int X = Integer.parseInt(st.nextToken());
-        
-        // 둘째 줄: N일 간의 방문자 수
+
         st = new StringTokenizer(br.readLine());
-        int[] visited = new int[N];
-        for (int i = 0; i < N; i++) {
-            visited[i] = Integer.parseInt(st.nextToken());
-        }
+        int []visited = new int[N];
+        for(int i = 0; i < N; i++) visited[i] = Integer.parseInt(st.nextToken());
 
-        // 1) 초기 윈도우 합(X일)
-        long windowSum = 0;
-        for (int i = 0; i < X; i++) {
-            windowSum += visited[i];
-        }
+        // sliding window
+        int totalSum = 0;
+        for(int i = 0; i < X; i++) totalSum += visited[i];
 
-        long maxValue = windowSum; // 최대 합
-        int count = 1;             // 최대 합을 가진 구간의 개수
-
-        // 2) 윈도우를 슬라이드(오른쪽 이동)
-        for (int start = 1; start <= N - X; start++) {
-            windowSum = windowSum - visited[start - 1] + visited[start + X - 1];
-
-            if (windowSum > maxValue) {
-                maxValue = windowSum;
-                count = 1;
-            } else if (windowSum == maxValue) {
-                count++;
+        int maxValue = totalSum;
+        int maxDays = 1;
+        for(int i = 1; i <= N - X; i++){
+            totalSum = totalSum - visited[i - 1] + visited[i + X - 1];
+            // 현재 값이 더 크면
+            if(maxValue < totalSum){
+                maxValue = totalSum;
+                maxDays = 1;
             }
+            // 현재 값이랑 최대값 같으면
+            else if (totalSum == maxValue) maxDays++;
         }
 
-        // 3) 결과 출력
-        if (maxValue == 0) {
-            System.out.println("SAD");
-        } else {
-            System.out.println(maxValue);
-            System.out.println(count);
+        if(maxValue == 0) bw.write("SAD");
+        else {
+            bw.write(Integer.toString(maxValue));
+            bw.write("\n");
+            bw.write(Integer.toString(maxDays));
         }
-
+        bw.flush();
+        bw.close();
         br.close();
     }
+
 }
