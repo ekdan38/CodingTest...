d5efd1948bde18c40e3daa3dfee20321f44b1f70
홍정화 <ekdan31@gmail.com>
2025-07-09 18:07:53 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16492 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -5,7 +5,7 @@ public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        // 두 재료의 번호가 M보다 크거나 같으면 된다.
+       
         // 투포인터 탐색
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
@@ -17,28 +17,23 @@ public class Main{
         }
 
         Arrays.sort(arr);
-
-//        for(int i = 1; i <= N; i++){
-//            System.out.print(" " + arr[i]);
-//        }
-//        System.out.println();
-
+        
         int start = 1;
-        int end = start + 1;
+        int end = N;
         int result = 0;
-
-        while(start < arr.length){
-            if(end >= arr.length) {
-                start++;
-                end = start + 1;
-            }
-            else if(arr[start] + arr[end] == M) {
-//                System.out.println(arr[start] + " +  " +  arr[end] + " = "  + (arr[start] + arr[end]));
+        int sum = 0;
+        while(start < end){
+            sum = arr[start] + arr[end];
+            if(sum == M){
                 result++;
-                end++;
+                start++;
+                end--;
             }
-            else{
-                end++;
+            else if(sum > M){
+                end--;
+            }
+            else if(sum < M){
+                start++;
             }
         }
 
