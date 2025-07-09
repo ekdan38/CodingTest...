a81cfd6a07c2d95724ffe6f168a525ec4d1a0643
홍정화 <ekdan31@gmail.com>
2025-07-09 13:04:06 +0900

    [Bronze I] Title: 평균, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,29 +1,28 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int subjects = Integer.parseInt(br.readLine());
+        
+        int N = Integer.parseInt(br.readLine());
+        double[] arr = new double[N];
+        
+        double M = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        List<Integer> scoreList = new ArrayList<>();
-
-        for(int i = 0; i < subjects; i++){
-            scoreList.add(Integer.parseInt(st.nextToken()));
+        for(int i = 0; i < N; i++){
+            arr[i] = Double.parseDouble(st.nextToken());
+            if (M < arr[i]) M = arr[i];
         }
-
-        int max = Collections.max(scoreList);
-
-        double result = 0;
-        for(int i = 0; i < subjects; i++){
-            result += Double.valueOf(scoreList.get(i)) / max * 100;
+        
+        double sum = 0;
+        for(int i = 0; i < N; i++){
+            sum += arr[i] / M * 100;
         }
-        bw.write(String.valueOf(result / subjects));
-
+        
+        double result = sum / N; 
+        bw.write(String.valueOf(result));
         bw.flush();
-        bw.close();
     }
 }
