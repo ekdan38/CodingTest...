cc6e6e1b99d878eaa02b1946b293428cbd08d0d2
홍정화 <ekdan31@gmail.com>
2025-07-11 01:33:36 +0900

    [Bronze I] Title: 세로읽기, Time: 108 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,32 +1,31 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-
+public class Main{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         char[][] arr = new char[5][15];
 
-        int max = 0;
-
-        for(int i = 0; i < 5; i++){
+        int maxLen = 0;
+        for(int i = 0; i <= 4; i++){
             String str = br.readLine();
-            if(str.length() >= max)max = str.length();
+            if (str.length() > maxLen) maxLen = str.length();
             for(int j = 0; j < str.length(); j++){
                 arr[i][j] = str.charAt(j);
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < max; i ++){
+        for(int i = 0; i < maxLen; i++){
             for(int j = 0; j < 5; j++){
-                if(arr[j][i] != '\u0000') sb.append(arr[j][i]);
+                char c = arr[j][i];
+                if(c != '\0') sb.append(c);
             }
         }
 
-        System.out.print(sb.toString());
+        bw.write(sb.toString());
+        bw.flush();
     }
-
-
 }
