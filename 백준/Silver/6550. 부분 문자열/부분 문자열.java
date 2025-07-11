731478e1f8c715d51adcf2f287dcac76552edb8b
홍정화 <ekdan31@gmail.com>
2025-07-11 18:28:55 +0900

    [Silver V] Title: 부분 문자열, Time: 144 ms, Memory: 16944 KB -BaekjoonHub

--- 백준/Silver/6550. 부분 문자열/부분 문자열.java
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.java
@@ -1,34 +1,37 @@
-import java.util.*;
+/**
+<문제 분석>
+    s,t 주어질때 t 에서 몇개 문자 제거하고 s가 부분 문자열이 되냐?
+    s 의 index 값 주고 t기준으로 for문 돌리면서, s[idx]와 t[i] 일치하면 idx ++;
+    끝나고나서 idx 값이 s.length() 이면 성공 
+*/
 import java.io.*;
 
-/**
- * 20154번
- * <문제 분석>
- *     s,t 주어진다.
- *     s 값 받아주고, t도 받아주고,
- *     s[0] == t[0 ~ N]
- *     s의 idx ++;
- */
 public class Main{
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        
+        StringBuilder sb = new StringBuilder();
         String str;
         while((str = br.readLine()) != null){
-            String s = str.split(" ")[0];
-            String t = str.split(" ")[1];
-
-            int sIdx = 0;
+            String[] splitedStr = str.split(" ");
+            String s = splitedStr[0];
+            String t = splitedStr[1];
+            
+            int idx = 0;
             for(int i = 0; i < t.length(); i++){
-                if(s.charAt(sIdx) == t.charAt(i)) sIdx++;
-                if(sIdx == s.length()) break;
+                if(s.charAt(idx) == t.charAt(i)) {
+                    idx++;
+                    if(idx == s.length()){
+                        sb.append("Yes").append("\n");
+                        break;
+                    }
+                }
             }
-
-            if(sIdx == s.length()) bw.write("Yes" + "\n");
-            else bw.write("No" + "\n");
+            
+            if(idx != s.length())sb.append("No").append("\n");
         }
-
+        bw.write(sb.toString());
         bw.flush();
     }
 }
