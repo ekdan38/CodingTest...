694ddaa5502560b639fd58bb85432a17e0db2581
홍정화 <ekdan31@gmail.com>
2025-07-14 11:40:00 +0900

    [Gold V] Title: 문자열 게임 2, Time: 540 ms, Memory: 22600 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -1,54 +1,39 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
 
-        while (T-- > 0) {
-            String W = br.readLine(); 
-            int K = Integer.parseInt(br.readLine()); 
-
-            int[] charCounts = new int[26];
-            for (int i = 0; i < W.length(); i++) {
-                charCounts[W.charAt(i) - 'a']++; 
+        while(T-- > 0){
+            String W = br.readLine();
+            int K = Integer.parseInt(br.readLine());
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            int[] arr = new int[26];
+            for(int i = 0; i < W.length(); i++){
+                arr[W.charAt(i) - 'a']++;
             }
 
-            int minLength = Integer.MAX_VALUE;
-            int maxLength = Integer.MIN_VALUE;
-
-            for (int i = 0; i < W.length(); i++) {
-                char currentChar = W.charAt(i);
-
-                if (charCounts[currentChar - 'a'] < K) {
-                    continue;
-                }
-
-                int count = 0; 
-                for (int j = i; j < W.length(); j++) {
-                    if (W.charAt(j) == currentChar) {
-                        count++;
-                    }
-
-                    if (count == K) {
-                        int currentLength = j - i + 1;
-                        minLength = Math.min(minLength, currentLength);
-                        maxLength = Math.max(maxLength, currentLength);
+            for(int i = 0; i < W.length(); i++){
+                if(arr[W.charAt(i) - 'a'] < K) continue;
+                int cnt = 0;
+                for(int j = i; j < W.length(); j++){
+                    if(W.charAt(j) == W.charAt(i)) cnt++;
+                    if(cnt == K){
+                        min = Math.min(min, j - i + 1);
+                        max = Math.max(max, j - i + 1);
                         break;
                     }
                 }
             }
-
-            if (minLength == Integer.MAX_VALUE || maxLength == Integer.MIN_VALUE) {
-                bw.write("-1\n"); 
-            } else {
-                bw.write(minLength + " " + maxLength + "\n");
-            }
+            bw.write(min != Integer.MAX_VALUE && max != Integer.MIN_VALUE ? min + " " + max +"\n" : "-1\n");
         }
         bw.flush();
- 
+        br.close();
+        bw.close();
     }
 }
