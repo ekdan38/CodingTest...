b37a3ab7a7c62adb26f7d9e7c208a4bb79fe6d95
홍정화 <ekdan31@gmail.com>
2025-09-05 17:30:26 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 540 ms, Memory: 294776 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,46 +1,34 @@
-// 1,2,3,4,5,6,7  k = 3
-// 3, 6  12457
-//
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        List<Integer> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> queue = new LinkedList<>();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        Queue<Integer> queue = new LinkedList<>();
-        int cnt = 0;
-        // 1회전
         for(int i = 1; i <= N; i++){
-            cnt++;
-            if(cnt % K == 0) list.add(i);
-            else queue.offer(i);
+            queue.offer(i);
         }
-        // 2회전 ~
-        if(!queue.isEmpty()){
-            while(true){
-                cnt++;
-                if(cnt % K == 0) list.add(queue.poll());
-                else queue.offer(queue.poll());
+        sb.append("<");
 
-                if(queue.isEmpty()) break;
+        while(!queue.isEmpty()){
+            for(int i = 0; i < K; i++){
+                if(i < K - 1) queue.offer(queue.poll());
+                else {
+                    sb.append(queue.poll());
+                    if(!queue.isEmpty()) sb.append(", ");
+                }
             }
         }
 
-        if(list.size() == 1) bw.write("<" + list.get(0) + ">");
-        else {
-            for(int i = 0; i < list.size(); i++){
-                if(i == 0) bw.write("<" + list.get(i));
-                else if(i == list.size() - 1) bw.write(", " + list.get(i) + ">");
-                else bw.write(", " + list.get(i));
-            }            
-        }
+        sb.append(">");
+        bw.write(sb.toString());
         bw.flush();
     }
 }
