1d7c85eadc022dc30e505c48ba95565045813137
홍정화 <ekdan31@gmail.com>
2025-09-05 20:21:22 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 45600 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -4,19 +4,21 @@ public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    
+
         int N = Integer.parseInt(br.readLine());
+
         Queue<Integer> queue = new LinkedList<>();
-        
-        for(int i = 1; i <= N; i++) queue.offer(i);
-        
-        while(queue.size() != 1){
+
+        for(int i = 1; i <= N; i++){
+            queue.offer(i);
+        }
+
+        while(queue.size() > 1){
             queue.poll();
             queue.offer(queue.poll());
         }
-        
+
         bw.write(String.valueOf(queue.poll()));
         bw.flush();
-        
     }
 }
