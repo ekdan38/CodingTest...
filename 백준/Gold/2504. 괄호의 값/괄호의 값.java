52ec6cd144572674f4bdc1f4af97f42f3e8928e3
홍정화 <ekdan31@gmail.com>
2025-09-09 14:07:20 +0900

    [Gold V] Title: 괄호의 값, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -1,65 +1,50 @@
 import java.io.*;
 import java.util.*;
-/**
- * 2504 괄호의 값
- *
- * 입력:
- *      1. 찻쩨 즐에 괄호열을 나타내는 문자열이 주어진다. (길이 : 1 ~ 30)
- *
- * 문제 분석:
- *      1. 4개의 기호 ‘(’, ‘)’, ‘[’, ‘]’를 이용해서 만들어지는 괄호열 중에서 올바른 괄호열 구해야함
- *      2.
- * 출력:
- *      1.
- *
- * */
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        char[] strArr = br.readLine().toCharArray();
+
+        String str = br.readLine();
         Stack<Character> stack = new Stack<>();
 
         int result = 0;
-        int midResult = 1;
-        for(int i = 0; i < strArr.length; i++){
-            char c = strArr[i];
-            if(c == '('){
-                stack.push(c);
-                midResult *= 2;
+        int mid = 1;
+        boolean chk = false;
+        for(int i = 0; i < str.length(); i++){
+            char current = str.charAt(i);
+            if(current == '('){
+                stack.push(current);
+                mid *= 2;
             }
-            else if(c == ')'){
-                // 잘못된 괄호열
-                if(stack.isEmpty() || stack.peek() != '(') {
-                    result = 0;
+            else if(current == '['){
+                stack.push(current);
+                mid *= 3;
+            }
+            else if (!stack.isEmpty()){
+                if(current == ')' && stack.peek() == '('){
+                    stack.pop();
+                    if(str.charAt(i - 1) == '(') result += mid;
+                    mid /= 2;
+                }
+                else if(current == ']' && stack.peek() == '['){
+                    stack.pop();
+                    if(str.charAt(i - 1) == '[') result += mid;
+                    mid /= 3;
+                }
+                else {
+                    chk = true;
                     break;
                 }
-                if(strArr[i - 1] == '(') result += midResult;
-                stack.pop();
-                midResult /= 2;
             }
-            else if(c == '['){
-                stack.push(c);
-                midResult *= 3;
-            }
-            else if(c == ']'){
-                // 잘못된 괄호열
-                if(stack.isEmpty() || stack.peek() != '[') {
-                    result = 0;                
-                    break;
-                }
-                if(strArr[i - 1] == '[') result += midResult;
-                stack.pop();
-                midResult /= 3;
+            else{
+                chk = true;
+                break;
             }
         }
-
-        if(!stack.isEmpty()) bw.write(Integer.toString(0));
-        else bw.write(Integer.toString(result));
+        if(chk || !stack.isEmpty()) bw.write("0");
+        else bw.write(String.valueOf(result));
         bw.flush();
-        bw.close();
-        br.close();
     }
 }
