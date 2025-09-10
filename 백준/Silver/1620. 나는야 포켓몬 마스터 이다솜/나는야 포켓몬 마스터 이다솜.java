e4cffbc2dc9a29e119d70ac3f93fa30b3f9184c7
홍정화 <ekdan31@gmail.com>
2025-09-10 13:20:51 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 464 ms, Memory: 57928 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,50 +1,34 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-/**
- * 1620 나는야 포켓몬 마스터 이다솜
- *
- * 입력:
- *      1. 첫째 줄에 N과 M (1 ~ 100,000)
- *      2. 둘째줄 부터 N개의 포켓몬 입력
- *      3. 이후 M 만큼 문제가 주어진다.
- *      => 숫자면 해당하는 포켓몬의 이름, 문자면 해당하는 포멧몬의 숫자
- *문제 분석:
- *      1. List 로 값 저장하고 출력 시키자. 최대 100,000개라 큰 문제 없다.
- *  출력:
- *      1. 숫자면 해당하는 포켓몬의 이름, 문자면 해당하는 포멧몬의 숫자
- */
-
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringBuilder result = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        HashMap<Integer, String> stringMap = new HashMap<>();
-        HashMap<String, Integer> integerMap = new HashMap<>();
-        for(int i = 1; i <= N; i++){
-            String str = br.readLine();
-            stringMap.put(i, str);
-            integerMap.put(str, i);
+        Map<String, Integer> map1 = new HashMap<>();
+        Map<Integer, String> map2 = new HashMap<>();
+        
+        int i = 1;
+        while(N --> 0){
+            String name = br.readLine();
+            map1.put(name, i);
+            map2.put(i, name);
+            i++;
         }
-
-        // 첫글자 대문자? 아스키 코드로 찾자
-        for(int i = 0; i < M; i++){
-            String str = br.readLine();
-            if(str.charAt(0) >= 65) result.append(integerMap.get(str)).append("\n");
-            else result.append(stringMap.get(Integer.parseInt(str))).append("\n");
+        
+        while(M --> 0){
+            String target = br.readLine();
+            if(target.charAt(0) >= 'A') sb.append(map1.get(target));
+            else sb.append(map2.get(Integer.parseInt(target)));
+            sb.append("\n");
         }
-
-        bw.write(result.toString());
+        bw.write(sb.toString());
         bw.flush();
-        br.close();
-        bw.close();
     }
-
 }
