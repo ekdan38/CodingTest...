1c2b98d6495c796c069e80ed648f590bfbf4bedc
홍정화 <ekdan31@gmail.com>
2025-09-15 22:57:24 +0900

    [Silver I] Title: 미로 탐색, Time: 104 ms, Memory: 14804 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,64 +1,58 @@
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 public class Main{
-    static boolean[][] visited;
-    static int[][] graph;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int N;
-    static int M;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[N + 1][M + 1];
-        graph = new int [N + 1][M + 1];
-
+        int[][] graph = new int[N + 1][M + 1];
         for(int i = 1; i <= N; i++){
             String str = br.readLine();
             for(int j = 1; j <= M; j++){
                 graph[i][j] = str.charAt(j - 1) - '0';
             }
         }
+        boolean[][] visited = new boolean[N + 1][M + 1];
+        int[] dx = {-1, 0, 0, 1};
+        int[] dy = {0, -1, 1, 0};
 
-        bw.write(String.valueOf(bfs()));
-        bw.flush();
-    }
-    static int bfs(){
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(1,1, 0));
+        Queue<Miro> queue = new LinkedList<>();
+        queue.offer(new Miro(1, 1, 1));
         visited[1][1] = true;
 
         while(!queue.isEmpty()){
-            Node current = queue.poll();
-            if(current.x == N && current.y == M) return current.d + 1;
+            Miro currentMiro = queue.poll();
+            int currentX = currentMiro.x;
+            int currentY = currentMiro.y;
+            int currentD = currentMiro.d;
+            if(currentX == N && currentY == M) {
+                bw.write(String.valueOf(currentD));
+                break;
+            }
             for(int i = 0; i < 4; i++){
-                int nX = current.x + dx[i];
-                int nY = current.y + dy[i];
+                int nextX = dx[i] + currentX;
+                int nextY = dy[i] + currentY;
 
-                if(nX >= 1 && nX <= N && nY >= 1 && nY <= M && graph[nX][nY] == 1 && !visited[nX][nY]){
-                    queue.offer(new Node(nX, nY, current.d + 1));
-                    visited[nX][nY] = true;
+                if(nextX >= 1 && nextX <= N && nextY >= 1 && nextY <= M && !visited[nextX][nextY] && graph[nextX][nextY] == 1){
+                    visited[nextX][nextY] = true;
+                    queue.offer(new Miro(nextX, nextY, currentD + 1));
                 }
             }
         }
-        return -1;
+        bw.flush();
     }
-
-    static class Node{
+    static class Miro{
         int x;
         int y;
         int d;
-        Node(int x, int y, int d){
+        Miro(int x, int y, int d){
             this.x = x;
             this.y = y;
             this.d = d;
         }
     }
-
 }
