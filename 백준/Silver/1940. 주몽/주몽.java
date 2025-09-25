09a589320431a176be442016ebab82c3cae613ea
홍정화 <ekdan31@gmail.com>
2025-09-25 18:21:59 +0900

    [Silver IV] Title: 주몽, Time: 168 ms, Memory: 16600 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,44 +1,38 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-       
-        // 투포인터 탐색
+
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int[] arr = new int[N + 1];
-        for(int i = 1; i <= N; i++){
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        Arrays.sort(arr);
         
-        int start = 1;
-        int end = N;
-        int result = 0;
-        int sum = 0;
+        Arrays.sort(arr);
+        int start = 0;
+        int end = N - 1;
+        int cnt = 0;
+        
         while(start < end){
-            sum = arr[start] + arr[end];
+            int sum = arr[start] + arr[end];
             if(sum == M){
-                result++;
+                cnt++;
                 start++;
-                end--;
-            }
-            else if(sum > M){
                 end--;
             }
             else if(sum < M){
                 start++;
             }
+            else if(sum > M){
+                end--;
+            }
         }
-
-        bw.write(String.valueOf(result));
+        bw.write(String.valueOf(cnt));
         bw.flush();
-
     }
 }
