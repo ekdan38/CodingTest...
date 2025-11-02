f3199936fce657685152b0c112a660d94adf4ab8
홍정화 <ekdan31@gmail.com>
2025-11-02 22:13:10 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 996 ms, Memory: 173132 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,30 +1,28 @@
-import java.io.*;
 import java.util.*;
- 
-public class Main {
- 
-	public static void main(String[] args) throws IOException {
-        
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		for(int i = 0; i < N; i++) {
-			int key = Integer.parseInt(st.nextToken());
-			map.put(key, map.getOrDefault(key, 0) + 1);
-		}
-		
-		int M = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		st = new StringTokenizer(br.readLine(), " ");
-        
-		for(int i = 0; i < M; i++) {
-			int key = Integer.parseInt(st.nextToken());
-			sb.append(map.getOrDefault(key, 0)).append(' ');
-		}
-		System.out.println(sb);
-	}
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while(N --> 0){
+            int n = Integer.parseInt(st.nextToken());
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        while(M --> 0){
+            int n = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(n, 0)).append(" ");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
 }
