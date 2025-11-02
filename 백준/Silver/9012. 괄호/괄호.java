fd8dc29610ef14566dce5a8c277b7a99ffdd3622
홍정화 <ekdan31@gmail.com>
2025-11-02 17:33:05 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,34 +1,33 @@
-/**
- ( 이면 push
- ) 이면 pop, empty라면 바로 중단하고 no
- 마지막 인덱스까지 갔는데 empty아니라면 no
- */
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
 
         int T = Integer.parseInt(br.readLine());
-        while(T -- > 0){
+        while(T --> 0){
             Stack<Character> stack = new Stack<>();
-            boolean midCheck = false;
-            String str = br.readLine();
-            for(int i = 0; i < str.length(); i++){
-                if(str.charAt(i) == '(') stack.push('(');
-                else{
-                    if(stack.isEmpty()) {
-                        midCheck = true;
+            boolean isVPS = true;
+            String input = br.readLine();
+            for(int i = 0; i < input.length(); i++){
+                char current = input.charAt(i);
+                if(current == '(') stack.push(current);
+                else if(current == ')') {
+                    if(stack.isEmpty()){
+                        isVPS = false;
                         break;
                     }
                     else stack.pop();
                 }
             }
-            if(midCheck) bw.write("NO\n");
-            else bw.write(stack.isEmpty() ? "YES\n" : "NO\n");
+            if(!isVPS || !stack.isEmpty()) sb.append("NO");
+            else sb.append("YES");
+            sb.append("\n");
         }
+        bw.write(sb.toString());
         bw.flush();
     }
 }
