e9c2db76503f307e117152902fb081941011b61a
홍정화 <ekdan31@gmail.com>
2025-11-03 18:56:55 +0900

    [Silver I] Title: 절댓값 힙, Time: 284 ms, Memory: 26564 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -4,34 +4,28 @@ public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        // X 가 0이면 절대값이 가장 작은 값 출력 후 제거
-        // X 가 0이 아니라면 배열에 넣는 연산
-        // 출력은 절댓값 가장 작은값 (절대값 작은게 여러개면 가장 ㅇ작은 수 출력)
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
-            if(Math.abs(a) == Math.abs(b)){
-                return Integer.compare(a, b);
+        Queue<Long> pq = new PriorityQueue<>((o1, o2) -> {
+            long absO1 = Math.abs(o1);
+            long absO2 = Math.abs(o2);
+            if(absO1 == absO2){
+                return Long.compare(o1, o2);
             }
-            return Integer.compare(Math.abs(a), Math.abs(b));
+            return Long.compare(absO1, absO2);
         });
 
-        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
         while(N --> 0){
-            int commend = Integer.parseInt(br.readLine());
-            if(commend == 0){
+            long x = Long.parseLong(br.readLine());
+            if(x == 0){
                 if(pq.isEmpty()) sb.append("0");
                 else sb.append(pq.poll());
                 sb.append("\n");
             }
-            else{
-                pq.offer(commend);
-            }
+            else pq.offer(x);
         }
-
         bw.write(sb.toString());
         bw.flush();
-
     }
 }
