7047abb963a2b484e0b0ec77622396a6e2832c95
홍정화 <ekdan31@gmail.com>
2025-11-05 18:22:27 +0900

    [Silver III] Title: 회전하는 큐, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,54 +1,42 @@
-import java.io.*;
 import java.util.*;
-public class Main {
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	static int N, M;
-	static StringBuilder sb = new StringBuilder();
-	static int count = 0;
-	static LinkedList<Integer> q = new LinkedList<>();
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		
-		int[] temp = new int[M];
-		for(int i = 0 ; i < M ; i++)
-			temp[i] = Integer.parseInt(st.nextToken());
+        // targetIdx 보다 deque.size()가 더 작으면...
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        LinkedList<Integer> deque = new LinkedList<>();
+        for(int i = 1; i <= N; i++){
+            deque.add(i);
+        }
 
-		for(int i = 1 ; i <= N ; i++)
-			q.add(i);
-		
-		for(int i = 0 ; i < M ; i++) {
-			
-			if(checkQueue(temp[i])) {
-				while(temp[i]!=q.get(0)) {
-				q.addLast(q.pollFirst());
-				count++;
-				}
-			}else {
-				while(temp[i]!=q.get(0)) {
-				q.addFirst(q.pollLast());
-				count++;
-				}
-			}
-			q.poll();
-		}
+        int cnt = 0;
+        st = new StringTokenizer(br.readLine());
+        while(M --> 0){
+            int target = Integer.parseInt(st.nextToken());
+            int targetIdx = deque.indexOf(target);
+            int midSize = deque.size() / 2;
 
-		System.out.println(count);
+            if(targetIdx <= midSize){
+                while(deque.peek() != target){
+                    deque.offerLast(deque.pollFirst());
+                    cnt++;
+                }
+            }
+            else{
+                while(deque.peek() != target){
+                    deque.offerFirst(deque.pollLast());
+                    cnt++;
+                }
+            }
+            deque.pollFirst();
+        }
+        bw.write(String.valueOf(cnt));
+        bw.flush();
 
-		}
-	public static boolean checkQueue(int a) {
-		for(int i = 0 ; i <= q.size()/2 ; i++) {
-			if(a == q.get(i))
-				return true;
-		}
-		return false;
-	}
-
+    }
 }
