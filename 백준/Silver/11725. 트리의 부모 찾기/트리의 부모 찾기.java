7044f6f4f4a6e82306a0afbf54e4097138cc5af1
홍정화 <ekdan31@gmail.com>
2025-11-11 17:40:09 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 528 ms, Memory: 61244 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,57 +1,54 @@
 import java.util.*;
 import java.io.*;
-
 public class Main{
     static List<List<Integer>> graph = new ArrayList<>();
-    static StringBuilder sb = new StringBuilder();
-    static int[] parents;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // bfs 돌리면서 다음 탐색 범위가 현재 노드의 자식 노드이다.
-        // 따라서 다음 탐색 범위의 부모는 현재 노드
         int N = Integer.parseInt(br.readLine());
-        parents = new int[N + 1];
+        int M = N - 1;
 
         for(int i = 0; i <= N; i++){
             graph.add(new ArrayList<>());
         }
 
-        StringTokenizer st;
-        for(int i = 0; i < N - 1; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+        while(M --> 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
 
-            graph.get(a).add(b);
-            graph.get(b).add(a);
+            graph.get(A).add(B);
+            graph.get(B).add(A);
         }
 
-        bfs(N, 1);
-        for(int i = 2; i < N + 1; i++){
-            sb.append(parents[i]).append("\n");
+        int[] arr = bfs(N);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 2; i < arr.length; i++){
+            sb.append(arr[i]).append("\n");
         }
         bw.write(sb.toString());
         bw.flush();
-
     }
-    static void bfs(int Node, int start){
-        boolean[] visited = new boolean[Node + 1];
+    static int[] bfs(int N){
+        int[] arr = new int[N + 1];
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
-        visited[start] = true;
+        queue.offer(1);
+
+        boolean[] visited = new boolean[N + 1];
+        visited[1] = true;
 
         while(!queue.isEmpty()){
             int current = queue.poll();
             for(int n : graph.get(current)){
                 if(!visited[n]){
-                    queue.offer(n);
                     visited[n] = true;
-                    parents[n] = current;
+                    queue.offer(n);
+                    arr[n] = current;
                 }
             }
         }
+        return arr;
     }
 }
