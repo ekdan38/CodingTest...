3c3df24cb17f4845ed4077b277c38143d008e21e
홍정화 <ekdan31@gmail.com>
2025-12-15 14:44:31 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1248 ms, Memory: 334540 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,63 +1,80 @@
 import java.util.*;
 import java.io.*;
 public class Main{
-    static List<List<Integer>> graph = new ArrayList<>();
-    static boolean[] visited;
+    static int N, M, K, X;
+    static List<List<Node>> graph = new ArrayList<>();
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        // 거리
+        K = Integer.parseInt(st.nextToken());
+        // 출발
+        X = Integer.parseInt(st.nextToken());
 
         for(int i = 0; i <= N; i++){
             graph.add(new ArrayList<>());
         }
-        visited = new boolean[N + 1];
 
-        for(int i = 0; i < M; i++){
+        while(M --> 0){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            graph.get(a).add(b);
+            graph.get(a).add(new Node(b, 1));
         }
 
-        List<Integer> list = bfs(X, K);
-        StringBuilder sb = new StringBuilder();
-        if(list.isEmpty()) sb.append("-1");
-        else {
+        List<Integer> list = dijkstra();
+        if(list.isEmpty()) sb.append(-1);
+        else{
             for(int n : list){
                 sb.append(n).append("\n");
             }
         }
         bw.write(sb.toString());
         bw.flush();
+
     }
-    static List<Integer> bfs(int start, int K){
+    static List<Integer> dijkstra(){
         List<Integer> list = new ArrayList<>();
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{start, 0});
-        visited[start] = true;
+        Queue<Node> queue = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
+        int[] dist = new int[N  + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        queue.offer(new Node(X, 0));
+        dist[X] = 0;
 
         while(!queue.isEmpty()){
-            int[] current = queue.poll();
-            int idx = current[0];
-            int dis = current[1];
+            Node cN = queue.poll();
 
-            for(int n : graph.get(idx)){
-                if(!visited[n]){
-                    queue.offer(new int[]{n, dis + 1});
-                    visited[n] = true;
-                    if(dis + 1 == K) list.add(n);
+            if(dist[cN.to] < cN.weight) continue;
+
+            for(Node n : graph.get(cN.to)){
+                if(dist[n.to] > n.weight + dist[cN.to]){
+                    dist[n.to] = n.weight + dist[cN.to];
+                    queue.offer(new Node(n.to, n.weight + dist[cN.to]));
                 }
             }
         }
-        Collections.sort(list);
+
+        for(int i = 1; i <= N; i++){
+            if(dist[i] == K) list.add(i);
+        }
+
         return list;
+    }
+    static class Node{
+        int to;
+        int weight;
+
+        public Node(int to, int weight){
+            this.to = to;
+            this.weight = weight;
+        }
     }
 }
