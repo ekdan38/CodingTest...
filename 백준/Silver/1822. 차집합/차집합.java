e72227638c9090a9fc3a102e4ad3c95f485fafc4
홍정화 <ekdan31@gmail.com>
2026-02-24 21:52:02 +0900

    [Silver IV] Title: 차집합, Time: 1544 ms, Memory: 215756 KB -BaekjoonHub

---
+++ 백준/Silver/1822. 차집합/차집합.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        TreeSet<Integer> set = new TreeSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < A; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+        
+        st = new StringTokenizer(br.readLine());
+        for (int j = 0; j < B; j++) {
+            int Bnum = Integer.parseInt(st.nextToken());
+            set.remove(Bnum);
+        }
+
+        sb.append(set.size()+"\n");
+        for (Integer num : set) {
+            sb.append(num+ " ");
+        }
+        System.out.println(sb);
+    }
+}
