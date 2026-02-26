837bf620a2eb6daee2e20ff401c2a72821cc2a7f
홍정화 <ekdan31@gmail.com>
2026-02-26 23:00:08 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 664 ms, Memory: 195680 KB -BaekjoonHub

---
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        final int MINUTE = Integer.parseInt(br.readLine());
+        
+        Stack<Homework> homeworkStack = new Stack<>();
+        int totalScore = 0;
+        for (int i = 0; i < MINUTE; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            if (st.nextToken().charAt(0) == '1') {
+                int score = Integer.parseInt(st.nextToken());
+                int minute = Integer.parseInt(st.nextToken());
+                if (minute == 1) {
+                    totalScore += score;
+                } else {
+                    homeworkStack.push(new Homework(score, minute - 1));
+                }
+            } else if (!homeworkStack.isEmpty()) {
+                Homework temp = homeworkStack.pop();
+                temp.minute--;
+                if (temp.minute == 0) {
+                    totalScore += temp.score;
+                } else {
+                    homeworkStack.push(temp);
+                }
+            }
+        }
+
+
+        sb.append(totalScore);
+        sb.append("\n");
+
+        bw.write(sb.toString());
+
+        bw.flush();
+        br.close();
+        bw.close();
+
+    }
+}
+
+class Homework {
+    int score;
+    int minute;
+
+    public Homework() {
+    }
+
+    public Homework(int score, int minute) {
+        this.score = score;
+        this.minute = minute;
+    }
+}
