9ce32718647adc8849e8f014491146a27696c87f
홍정화 <ekdan31@gmail.com>
2026-03-17 23:11:49 +0900

    [Silver II] Title: 한 줄로 서기, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N+1];
+        List<Integer> result = new LinkedList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = N; i >= 1; i--){
+            result.add(arr[i], i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int su : result){
+            sb.append(su).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
